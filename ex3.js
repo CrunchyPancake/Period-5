dbbf2e3df8456c26189bdad92a925bc2aa229712
Jonathan <jonathan.henriksen@yandex.com>
2017-11-26 23:15:17 +0100

    Optimized sorting

--- ex3.js
+++ ex3.js
@@ -1,42 +1,37 @@
 const fetch = require('node-fetch')
 const urls = [
     'https://jsonplaceholder.typicode.com/photos?albumId=1',
-    'https://jsonplaceholder.typicode.com/photos?albumId=2',
     'https://jsonplaceholder.typicode.com/photos?albumId=3',
-    'https://jsonplaceholder.typicode.com/photos?albumId=4',
     'https://jsonplaceholder.typicode.com/photos?albumId=5',
-    'https://jsonplaceholder.typicode.com/photos?albumId=6',
     'https://jsonplaceholder.typicode.com/photos?albumId=7',
-    'https://jsonplaceholder.typicode.com/photos?albumId=8',
-    'https://jsonplaceholder.typicode.com/photos?albumId=9',
+    'https://jsonplaceholder.typicode.com/photos?albumId=9'
+
 ]
 
-let asyncRequest = function (cb, wordCount, index = 0, result = []) {
-    let promises = urls.map(url => fetch(url).then(data => data.json()))
-    Promise.all(promises).then(results => { 
+let asyncRequest = function (cb, wordCount) {
+    let requests = urls.map(url => fetch(url).then(data => data.json()))
+    Promise.all(requests).then(results => {
         cb(sortData(results, wordCount))
     })
 }
 
 let sortData = (data, wordCount) => {
     let result = [];
-    for (let i = 0 ; i < data.length ; i++) {
-        for (let k = 0 ; k < data[i].length ; k++) {
+    for (let i = 0; i < data.length; i++) {
+        for (let k = 0; k < data[i].length; k++) {
             result.push(data[i][k]);
         }
     }
 
-    let filteredData = result.filter((el) => {
+    return result.filter((el) => {
         return el.title.split(" ").length === wordCount;
     })
-
-    return filteredData.map((el) => {
-        return {
-            id: el.id,
-            title: el.title
-        }
-    })
-
+        .map((el) => {
+            return {
+                id: el.id,
+                title: el.title
+            }
+        })
 }
 
 module.exports = { asyncRequest: asyncRequest };

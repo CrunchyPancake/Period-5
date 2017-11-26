ef1ec6bb7f5691b4157990224a6049f0659e1849
Jonathan <jonathan.henriksen@yandex.com>
2017-11-26 22:05:30 +0100

    Fixed excercise 3

--- ex3.js
+++ ex3.js
@@ -1,38 +1,42 @@
 const fetch = require('node-fetch')
 const urls = [
     'https://jsonplaceholder.typicode.com/photos?albumId=1',
+    'https://jsonplaceholder.typicode.com/photos?albumId=2',
     'https://jsonplaceholder.typicode.com/photos?albumId=3',
+    'https://jsonplaceholder.typicode.com/photos?albumId=4',
     'https://jsonplaceholder.typicode.com/photos?albumId=5',
+    'https://jsonplaceholder.typicode.com/photos?albumId=6',
     'https://jsonplaceholder.typicode.com/photos?albumId=7',
+    'https://jsonplaceholder.typicode.com/photos?albumId=8',
     'https://jsonplaceholder.typicode.com/photos?albumId=9',
 ]
 
 let asyncRequest = function (cb, wordCount, index = 0, result = []) {
-    if (index == urls.length - 1) {
-        return cb(result);
-    }
-
-    fetch(urls[index])
-        .then(response => {
-            response.json()
-                .then(data => {
-                    mergedResult = result.concat(sortData(data, wordCount));
-                    asyncRequest(cb, wordCount, index + 1, mergedResult);
-                })
-        })
+    let promises = urls.map(url => fetch(url).then(data => data.json()))
+    Promise.all(promises).then(results => { 
+        cb(sortData(results, wordCount))
+    })
 }
 
 let sortData = (data, wordCount) => {
-    let filteredData = data.filter((el) => {
+    let result = [];
+    for (let i = 0 ; i < data.length ; i++) {
+        for (let k = 0 ; k < data[i].length ; k++) {
+            result.push(data[i][k]);
+        }
+    }
+
+    let filteredData = result.filter((el) => {
         return el.title.split(" ").length === wordCount;
     })
 
-    let result = filteredData.map((el) => {
+    return filteredData.map((el) => {
         return {
             id: el.id,
-            title: el.title}
+            title: el.title
+        }
     })
-    return result;
+
 }
 
 module.exports = { asyncRequest: asyncRequest };
